02335670784fdf44ca3363087a0b73b61cc84114
Justin Barabander <justinbarabander@Justins-MacBook-Pro.local>
2015-08-19 13:32:57 -0400

    made index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+// var Place = models.Place;
+var Restaurant = models.Restaurant;
+var Activity = models.Activity;
+var Hotel = models.Hotel;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var promArr = [];
+  promArr.push(Restaurant.find({}).exec());
+  promArr.push(Activity.find({}).exec());
+  promArr.push(Hotel.find({}).exec());
+  Promise.all(promArr).then(function(results) {
+    res.render('index',{
+      restaurants: results[0],
+      activities: results[1],
+      hotels: results[2]
+    });
+  });
 });
 
 module.exports = router;
